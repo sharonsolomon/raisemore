a5a6ce16e251c298f5709ea0ab3cd6206662c153
zachblume <zachblume@gmail.com>
2023-03-28 15:31:06 -0400

    fix tailwind for /src/ directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             fontSize: { "2xl": "1.4rem" },
