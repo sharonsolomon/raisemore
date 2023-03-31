8bd78955f1ebb2f39f5f505d43c03d013b51f0fb
zachblume <zachblume@gmail.com>
2023-03-31 14:48:46 -0400

    refactor out case helper

--- src/lib/cases.js
+++ src/lib/cases.js
@@ -4,6 +4,9 @@
 var wordSeparators = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/;
 var capitals = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]/g;
 
+export const pluralize = (single, plural, number) => (number > 1 ? plural : single);
+export const capitalize = (s) => (s?.length > 0 ? s.charAt(0).toUpperCase() + s.slice(1) : "");
+
 function snakeCase(str) {
     //replace capitals with space + lower case equivalent for later parsing
     str = str.replace(capitals, function (match) {
