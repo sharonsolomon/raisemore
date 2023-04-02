37a0814f1215a4bff8daa1cc6882c31ff26a61ed
zachblume <zachblume@gmail.com>
2023-04-02 02:12:13 -0400

    empty rows shows no matching records

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,7 +8,10 @@ const Table = ({ rows, columns, rowCount, onPageChange, page }) => {
             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="min-w-full py-2 align-middle sm:px-6 lg:px-8">
                     <div className="shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg w-full overflow-hidden">
-                        <div className="overflow-scroll" style={{ height: "56vh" }}>
+                        <div
+                            className="overflow-scroll"
+                            style={rows?.length ? { height: "56vh" } : {}}
+                        >
                             <table className="w-full table-fixed min-w-full divide-y divide-gray-300">
                                 <thead className="bg-gray-50">
                                     <tr>
