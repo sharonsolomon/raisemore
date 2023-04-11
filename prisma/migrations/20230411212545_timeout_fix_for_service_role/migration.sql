866b6a85a89cc5dce1d7eefee84d9b75a83b71af
zachblume <zachblume@gmail.com>
2023-04-11 17:35:55 -0400

    fixed timeout issues

---
+++ prisma/migrations/20230411212545_timeout_fix_for_service_role/migration.sql
@@ -0,0 +1,4 @@
+alter role service_role set statement_timeout to 120000;
+alter role authenticator set statement_timeout to 120000;
+alter role authenticated set statement_timeout to 120000;
+alter role anon set statement_timeout to 120000;
